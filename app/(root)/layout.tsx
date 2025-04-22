2a081031c7ca648d57330c6f67e3e8d01834730d
Boy-Zee <boyzee1266@gmail.com>
2025-04-22 09:00:10 +0100

    removed the auth comments

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,9 +5,9 @@ import { redirect } from 'next/navigation'
 import React, { ReactNode } from 'react'
 
 const RootLayout = async ({children}: {children: ReactNode}) => {
-  // const isUserAuthenticated = await isAuthenticated()
+  const isUserAuthenticated = await isAuthenticated()
 
-  // if(!isUserAuthenticated) redirect('/sign-in')
+  if(!isUserAuthenticated) redirect('/sign-in')
 
   return (
     <div className='root-layout'>
